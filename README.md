3ed455a4bdf103359150a930768f990d9c48a71a
StarSolution1221 <135289424+StarSolution1221@users.noreply.github.com>
2023-08-16 15:19:01 +0300

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Scrap-Samples
 ## Scrap.py
+
+scrap the datas for basketball reference table from https://ibasketball-reference.com and store to "mvp" folder
+
+## Scraping.py
+
+scrap the price datas from https://isthereanydeal.com
--- Scraping.py
+++ Scraping.py
@@ -1,19 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-
 Game = "sonicfrontiers"
-
 
 html = requests.get("https://isthereanydeal.com/game/" + Game + "/info/")
 soup = BeautifulSoup(html.text, "lxml")
 
-
 price_panels = soup.find_all("td", class_="priceTable__new")
-
 
 prices = [panel.text.strip() for panel in price_panels]
 
+# remove duplicate values in prices list
+prices= set(prices)
 
 for price in prices:
     print(price)
